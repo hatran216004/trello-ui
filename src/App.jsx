36305a5aa181178aaf30469ce675c5c32be6af7c
hatran216004 <qiqisokiu.321@gmail.com>
2024-07-19 08:39:39 +0700

    Dark Mode & Light Mode #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
-import { useColorScheme } from '@mui/material/styles'
-
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme()
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light')
-      }}
-    >
-      {mode === 'light' ? 'Dark Mode' : 'Light Mode'}
-    </Button>
-  )
-}
+import ModeSelect from './components/ModeSelect'
 
 function App() {
   return (
     <>
-      <ModeToggle />
+      <ModeSelect />
+      <hr />
       <Typography variant="body2" color="text.secondary">
         h1. Heading
       </Typography>
